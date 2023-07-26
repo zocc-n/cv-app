0f3b2e1b8e53bec1ef4e88e31ecf377ede448065
zocc-n <znanic@yahoo.com>
2023-07-26 13:50:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CV Web App
+
+This web application was done as part of The Odin Project's React course.
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    console.log('')
+  )
+}
+
+export default App
